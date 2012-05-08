c88efb7d86e3aee612a6d16925cab98686e9c558
Coin Mint <coinmint@i2pmail.org>
2012-05-08 05:53:29 +0200

    Added the previously missed scripts for building. Update gitignore for results of php5 build and of the scripts (dependencies)

---
+++ build-php-debian-local.sh
@@ -0,0 +1,1 @@
+make test_wallet php5 install LOCAL_INCLUDEDIRS=-I/home/ot/local/include LOCAL_LIBDIRS=-L/home/ot/local/lib EXECUTABLE_INSTALL_FOLDER=/home/ot/local/bin LIBRARY_INSTALL_FOLDER=/home/ot/local/lib

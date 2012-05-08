c382fefd1a9e9973af8a4998853db6f066073396
Coin Mint <coinmint@i2pmail.org>
2012-05-08 20:32:51 +0200

    Added reminder message to export LD_LIBRARY_PATH on local build

--- build-php-debian-local.sh
+++ build-php-debian-local.sh
@@ -1,1 +1,12 @@
-make test_wallet php5 install LOCAL_INCLUDEDIRS=-I/home/ot/local/include LOCAL_LIBDIRS=-L/home/ot/local/lib EXECUTABLE_INSTALL_FOLDER=/home/ot/local/bin LIBRARY_INSTALL_FOLDER=/home/ot/local/lib DYNAMIC_OTLIB=1
+#!/bin/bash -e
+
+localdir=${HOME}/local/
+
+make test_wallet php5 install LOCAL_INCLUDEDIRS=-I${localdir}include LOCAL_LIBDIRS=-L${localdir}lib EXECUTABLE_INSTALL_FOLDER=${localdir}bin LIBRARY_INSTALL_FOLDER=${localdir} DYNAMIC_OTLIB=1 && (
+echo ""
+echo ""
+echo "Done - builded for use in local dir=${localdir}" 
+echo "Remember to do:   export LD_LIBRARY_PATH=${localdir}lib   before running binaries created in ${localdir}bin like ot_server or ot (client) there."
+echo ""
+echo ""
+)
